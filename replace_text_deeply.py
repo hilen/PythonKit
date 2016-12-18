f87d11e78e2d81c2f846b9c48210bd69abbf3178
龙桢 <hilenkz@gmail.com>
2016-12-18 20:45:54 +0800

    update replace_text_deeply.py

--- replace_text_deeply.py
+++ replace_text_deeply.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import fileinput
+import sys
 import os
-from os import walk
-import os
-import fileinput, sys
 
-def replace_text_in_path_deeply(startpath,nowText,targetText):
-for file in os.listdir(startpath):
-	for line in fileinput.input(file, inplace=True):
-		line = line.replace(targetText, nowText)
-		sys.stdout.write(line)
+def replace_text_in_path_deeply(folderPath,searchExp,replaceExp):
+	for file in os.listdir(folderPath):
+	    for line in fileinput.input(file, inplace=1):
+	        if searchExp in line:
+	            line = line.replace(searchExp,replaceExp)
+	        sys.stdout.write(line)
 
 replace_text_in_path_deeply("路径","现在的字符","替换的字符")
